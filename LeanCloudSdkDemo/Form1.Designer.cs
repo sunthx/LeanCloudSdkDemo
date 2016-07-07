5a8f1850db2d415410e910957658dedf0d04b2e6
sunth <sunth0101@126.com>
2016-07-07 15:21:09 +0800

    LeanCloud doNet SDK Demo

---
+++ LeanCloudSdkDemo/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace LeanCloudSdkDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtAppId = new System.Windows.Forms.TextBox();
+            this.txtAppKey = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(12, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "!!!初始化AVClient";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtAppId
+            // 
+            this.txtAppId.Location = new System.Drawing.Point(13, 13);
+            this.txtAppId.Name = "txtAppId";
+            this.txtAppId.Size = new System.Drawing.Size(326, 21);
+            this.txtAppId.TabIndex = 1;
+            // 
+            // txtAppKey
+            // 
+            this.txtAppKey.Location = new System.Drawing.Point(345, 13);
+            this.txtAppKey.Name = "txtAppKey";
+            this.txtAppKey.Size = new System.Drawing.Size(332, 21);
+            this.txtAppKey.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "添加对象";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 41);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "修改对象";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 143);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(151, 41);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "删除对象";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 366);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(151, 41);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "查询对象";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 284);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(151, 41);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "获取对象";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 190);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(151, 41);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "删除对象的一个属性";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 413);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(151, 41);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "获取对象的个数";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(176, 366);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(151, 41);
+            this.button9.TabIndex = 4;
+            this.button9.Text = ">=  与 <=";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(345, 413);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(151, 41);
+            this.button10.TabIndex = 4;
+            this.button10.Text = "ContainIn";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(13, 507);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(151, 41);
+            this.button11.TabIndex = 4;
+            this.button11.Text = "Limit";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(13, 460);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(151, 41);
+            this.button12.TabIndex = 4;
+            this.button12.Text = "Skip";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtInfo);
+            this.groupBox1.Location = new System.Drawing.Point(170, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(507, 276);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Info";
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(6, 20);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInfo.Size = new System.Drawing.Size(495, 250);
+            this.txtInfo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 343);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(671, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "---------------------------------------------------------------------------------" +
+    "------------------------------";
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(176, 413);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(151, 41);
+            this.button13.TabIndex = 4;
+            this.button13.Text = "== 与 !=";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(345, 366);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(151, 41);
+            this.button14.TabIndex = 4;
+            this.button14.Text = "Contains";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(345, 460);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(151, 41);
+            this.button15.TabIndex = 4;
+            this.button15.Text = "NotContainIn";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(511, 366);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(151, 41);
+            this.button16.TabIndex = 4;
+            this.button16.Text = "StartsWith";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(511, 413);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(151, 41);
+            this.button17.TabIndex = 4;
+            this.button17.Text = "EndWith";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 568);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtAppKey);
+            this.Controls.Add(this.txtAppId);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LeanCloudSdkDemo";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtAppId;
+        private System.Windows.Forms.TextBox txtAppKey;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+    }
+}
+
---
+++ LeanCloudSdkDemo/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AVOSCloud;
+
+namespace LeanCloudSdkDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string _objectId = string.Empty;
+        private string _queryClassName = "persons";
+
+        #region Private
+
+        private void Message(string value, bool add = false)
+        {
+            if (!add)
+                this.txtInfo.Text = value;
+            else
+                this.txtInfo.Text += value + "\r\n";
+        }
+
+        private void Clear()
+        {
+            this.txtInfo.Text = string.Empty;
+        }
+
+        private async Task<string> AddTestDataItem(string className = "person")
+        {
+            string[] strings = new[] {"Tom", "Jim", "John", "Michelle", "Amy", "Mary", "Kim"};
+            int[] ints = new[] {20, 12, 22, 23, 25, 26, 50, 65, 35, 40, 10, 100};
+
+            string objectId = string.Empty;
+            AVObject person = new AVObject(className)
+            {
+                ["name"] = strings[new Random().Next(0, 5)],
+                ["age"] = ints[new Random().Next(0, 11)]
+            };
+
+            await person.SaveAsync().ContinueWith(t => { objectId = person.ObjectId; });
+
+            return objectId;
+        }
+
+        private async Task AddTestDataItems(int count = 10, string className = "person")
+        {
+            for (int i = 0; i < count; i++)
+            {
+                await AddTestDataItem(className);
+            }
+        }
+
+        private async Task AddTestData()
+        {
+            await AddTestDataItems(50, _queryClassName);
+        }
+
+        #endregion
+
+        #region Add Delete Update
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            AVClient.Initialize(txtAppId.Text, txtAppKey.Text);
+//            await AddTestData();
+            Message("Initialize Ok");
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            _objectId = await AddTestDataItem();
+            Message("[Insert] 成功添加一条数据。[objectId] =" + _objectId);
+        }
+
+        private async void button6_Click(object sender, EventArgs e)
+        {
+            _objectId = await AddTestDataItem();
+            AVObject sunth = AVObject.CreateWithoutData("person", _objectId);
+            await sunth.FetchAsync();
+
+            Message($"[Get] 获取一条数据成功,[objectId] = {_objectId} && [name] = {sunth["name"]}");
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            //先添加一条数据，然后删除。
+            _objectId = await AddTestDataItem();
+
+            AVObject sunth = AVObject.CreateWithoutData("person", _objectId);
+            await sunth.DeleteAsync();
+
+            Message("[Delete] 删除一条数据成功 , [ObjectId] = " + _objectId);
+        }
+
+        private async void button7_Click(object sender, EventArgs e)
+        {
+            //先添加一条数据，然后删除AVObject中的一个属性，这里的删除指的是置空。
+            _objectId = await AddTestDataItem();
+
+            AVObject sunth = AVObject.CreateWithoutData("person", _objectId);
+            sunth.Remove("age");
+            await sunth.SaveAsync();
+            Message("[Delete] 删除 [Person] 中 [age] 属性成功, [ObjectId] = " + _objectId);
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            //先添加一条数据，然后修改。
+            _objectId = await AddTestDataItem();
+
+            AVObject sunth = AVObject.CreateWithoutData("person", _objectId);
+            sunth["name"] = "new sunth";
+            sunth["age"] = 26;
+            await sunth.SaveAsync();
+
+            Message("[Update] 更新数据成功, [ObjectId] = " + _objectId);
+        }
+
+        #endregion
+
+        // ---- 查询
+
+        private async void button5_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+            AVQuery<AVObject> query = new AVQuery<AVObject>(_queryClassName);
+            var result = await query.FindAsync();
+            if (!result.Any())
+            {
+                Message("[Query] No Data !");
+                return;
+            }
+
+            result.ToList().ForEach((item) => { Message($"[Query] ObjectId = {item.ObjectId} , Name = {item["name"]} , Age = {item["age"]}", true); });
+        }
+
+        private async void button8_Click(object sender, EventArgs e)
+        {
+            AVQuery<AVObject> query = new AVQuery<AVObject>(_queryClassName);
+            var result = await query.CountAsync();
+            Message($"[Query] Items Count = {result}");
+        }
+
+        private async void button9_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+            Message($"[Query] 操作符 >= ", true);
+            AVQuery<AVObject> query = new AVQuery<AVObject>(_queryClassName);
+            var result = await query.WhereGreaterThanOrEqualTo("age", 26).FindAsync();
+            Message($"[Query] WhereGreaterThanOrEqualTo('age', 26) 满足条件的个数 : {result.Count()}", true);
+
+            Message($"[Query] 操作符 <= ", true);
+            query = new AVQuery<AVObject>(_queryClassName);
+            result = await query.WhereLessThanOrEqualTo("age", 26).FindAsync();
+            Message($"[Query] WhereLessThanOrEqualTo('age', 26) 满足条件的个数 : {result.Count()}", true);
+        }
+
+        //Skip
+        private async void button12_Click(object sender, EventArgs e)
+        {
+            var query = new AVQuery<AVObject>(_queryClassName);
+
+            //使用Skip函数可以实现分页功能,对方法CountAsync无效果。
+            Message($"[Skip] Items Count ： {await query.CountAsync()}");
+
+            var result = await query.Skip(10).FindAsync();
+
+            Message($"[Skip] 调用 query.Skip(10) 后结果集的 Count ： {result.Count()}");
+        }
+
+        //Limit
+        private async void button11_Click(object sender, EventArgs e)
+        {
+            //限制查询的数量
+            var result = await new AVQuery<AVObject>(_queryClassName).Limit(10).FindAsync();
+            Message($"[Skip] 调用 query.Limit(10) 后结果集的 Count ： {result.Count()}");
+        }
+
+        private async void button13_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+            Message($"[Query] 操作符 == ", true);
+            AVQuery<AVObject> query = new AVQuery<AVObject>(_queryClassName);
+            var result = await query.WhereEqualTo("age", 26).FindAsync();
+            Message($"[Query] WhereEqualTo('age', 26) 满足条件的个数 : {result.Count()}", true);
+
+            Message($"[Query] 操作符 != ", true);
+            query = new AVQuery<AVObject>(_queryClassName);
+            result = await query.WhereNotEqualTo("age", 26).FindAsync();
+            Message($"[Query] WhereNotEqualTo('age', 26) 满足条件的个数 : {result.Count()}", true);
+        }
+
+        private async void button17_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+            AVQuery<AVObject> query = new AVQuery<AVObject>(_queryClassName);
+            var result = await query.WhereEndsWith("name", "m").FindAsync();
+            Message($"[Query] WhereStartsWith('name', 'm') 满足条件的个数 : {result.Count()}", true);
+        }
+
+        private async void button16_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+            AVQuery<AVObject> query = new AVQuery<AVObject>(_queryClassName);
+            var result = await query.WhereStartsWith("name", "J").FindAsync();
+            Message($"[Query] WhereStartsWith('name', 'J') 满足条件的个数 : {result.Count()}", true);
+        }
+
+        private async void button14_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+            AVQuery<AVObject> query = new AVQuery<AVObject>(_queryClassName);
+            var result = await query.WhereContains("name", "J").FindAsync();
+            Message($"[Query] WhereContains('name', 'J') 满足条件的个数 : {result.Count()}", true);
+        }
+
+        private async void button10_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+            string[] conditions = {"Tom","Jim"};
+            AVQuery<AVObject> query = new AVQuery<AVObject>(_queryClassName);
+            var result = await query.WhereContainedIn("name", conditions).FindAsync();
+
+            Message($"[Query] WhereContainedIn('name', 'J') 满足条件的个数 : {result.Count()}", true);
+        }
+
+        private async void button15_Click(object sender, EventArgs e)
+        {
+            Clear();
+
+            string[] conditions = { "Tom", "Jim" };
+
+            AVQuery<AVObject> query = new AVQuery<AVObject>(_queryClassName);
+            var result = await query.WhereNotContainedIn("name", conditions).FindAsync();
+            Message($"[Query] WhereNotContainedIn('name', 'J') 满足条件的个数 : {result.Count()}", true);
+        }
+
+    }
+}
